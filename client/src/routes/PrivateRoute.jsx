289e4161f29a39d05e6942817438b407e786fef6
gajendranasokkumar <gajendran2908@gmail.com>
2025-04-14 09:30:17 +0530

    uncommented the protected route

--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element: Component, token }) => {
-//   return token ? Component : <Navigate to="/" />;
-  return token ? Component : Component;
+  return token ? Component : <Navigate to="/" />;
+  // return token ? Component : Component;
 };
 
 export default PrivateRoute;
